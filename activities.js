56e4fcbe26c2484f4e8d913ad4001b20af0f6105
Chris Aguero <chrismatthew176@yahoo.com>
2017-12-19 14:01:59 -0800

    Updated activities.js

--- activities.js
+++ activities.js
@@ -1,4 +1,4 @@
-activities = [
+let activities = [
     [ "Activity", "Hour", "% of Day"],
     [ "Sleeping", 8],
     [ "Working", 8],
@@ -6,20 +6,13 @@ activities = [
     [ "Eating", 2]
 ]
 
-function percent (activities) {
-    for ( i = 1; i <= 4; i+=1){
-       a = activities [i][1];
+function findPercent (activities) {
+    for ( i = 1; i <= activities.length -1; i+=1){
+       let a = activities [i][1];
        percentage = (a / 24) * 100;
-       activities[i][2] = Math.round(percentage);
+       activities[i][2] = Math.round(percentage) + "%";
     }
 }
 
-function listActivity (activities){
-    for (i = 0; i <= 4; i+=1){
-        console.log(activities[i][0] + " " + activities[i][2]);
-    }
-}
-
-percent(activities);
-listActivity(activities);
-
+findPercent(activities);
+console.log(activities);
